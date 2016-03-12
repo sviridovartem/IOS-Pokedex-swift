e8360d0237417cdde660a6595939e79e4b80e12a
Admin <admin@Mac-Admin.local>
2016-03-12 23:58:42 +0200

    included music

--- Pokedex-by-Sviridov/ViewController.swift
+++ Pokedex-by-Sviridov/ViewController.swift
@@ -7,18 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
     @IBOutlet weak var collection: UICollectionView!
     
     var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self
         collection.dataSource = self
         parsePokemosCSV()
+    }
+    
+    func initAudio(){
+        let path = NSBundle.mainBundle().pathForResource("9002", ofType: "mp3")!
+        
+        do {
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        } catch let err as NSError {
+            print (err.debugDescription)
+        }
     }
     
     func parsePokemosCSV(){
@@ -73,7 +89,15 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         
         return CGSizeMake(105, 105)
     }
-    @IBAction func musicBtnPressed(sender: AnyObject) {
+    @IBAction func musicBtnPressed(sender: UIButton!) {
+        
+        if musicPlayer.playing{
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        } else {
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
     }
     
 }
