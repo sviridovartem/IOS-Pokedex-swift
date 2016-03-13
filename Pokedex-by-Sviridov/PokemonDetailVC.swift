012aa01a7e32ae6cffbe245480d150ea806204e2
Admin <admin@Mac-Admin.local>
2016-03-13 22:09:48 +0200

    UIStackView done

--- Pokedex-by-Sviridov/PokemonDetailVC.swift
+++ Pokedex-by-Sviridov/PokemonDetailVC.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController{
-
+    @IBOutlet weak var nameLbl:UILabel!
     var pokemon:Pokemon!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nameLbl.text = pokemon.name
     }
 }
